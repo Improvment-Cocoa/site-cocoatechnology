9a373976656db7ebd8502856dc549c9ce8ea4763
JuliaHown <julia.santos@sptech.school>
2023-04-17 14:39:31 -0300

    Atualização do simulador

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -8,7 +8,7 @@ function simulador() {
     var qnt_hectares = Number(input_hectares.value);
     var sacas_produzidas = Number(input_sacas.value);
     var valor_saca = Number(input_valorsacas.value);
-    var despesas = Number(input_despezas.value);
+    var despesas = Number(input_despesas.value);
 
     var semCocoa = (sacas_produzidas * valor_saca) - despesas;
     var perdaHectare = (qnt_hectares * 10000) * 0.30;
@@ -19,14 +19,14 @@ function simulador() {
 
 
     div_resulCalculo.innerHTML = ` 
-    
+    <div class="container-resultado">
     <h1 style="font-weight: 700; font-size: 21px;">Sem a solução da Cocoa Technology</h1><br>
     <p style="font-size: 17px;">O valor de retorno será de R$${semCocoa}.</p>
     <p style="font-size: 17px;">Perda de <span style="color: red;">${perdaHectare}</span> (M²) da produtividade na plantação!</p><br>
 
     <h1 style="font-weight: 700; font-size: 21px; margin-top: 5vh;">Com a solução da Cocoa Technology</h1><br>
     <p style="font-size: 17px;">O valor de retorno será de <span style="color: green;">R$${comCocoa}</span></p>
-    <p style="font-size: 17px;">Obtendo um aumento no lucro de <span style="color: green;">R$${lucro}</span></p>`;
+    <p style="font-size: 17px;">Obtendo um aumento no lucro de <span style="color: green;">R$${lucro}</span></p> </div>`;
 }
 
 window.addEventListener("scroll", rolagem);
