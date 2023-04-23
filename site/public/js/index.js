deaf63f4d612f86eba390ded5dbaf8f7054ba0d3
annamatos <anna.matos@sptech.school>
2023-04-23 19:32:15 -0300

    Adicionando alert no simulador

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -16,7 +16,9 @@ function simulador() {
     var comCocoa = (sacas_produzidas * 1.25) * valor_saca - despesas;
     var lucro = comCocoa - semCocoa;
 
-
+    if(semCocoa < 0 || comCocoa < 0 ){
+        alert(`Erro na entrada de dados! Verifique novamente suas informações, as despesas estão excedendo o lucro.`)
+    } else {
     div_resulCalculo.innerHTML = ` 
     <div class="container-resultado">
     <h1 style="font-weight: 700; font-size: 21px;">Sem a solução da Cocoa Technology</h1><br>
@@ -26,6 +28,7 @@ function simulador() {
     <h1 style="font-weight: 700; font-size: 21px; margin-top: 5vh;">Com a solução da Cocoa Technology</h1><br>
     <p style="font-size: 17px;">O valor de retorno será de <span style="color: green;">R$${comCocoa}</span></p>
     <p style="font-size: 17px;">Obtendo um aumento no lucro de <span style="color: green;">R$${lucro}</span></p> </div>`;
+    }
 }
 
 window.addEventListener("scroll", rolagem);
